2332adfc967b470e31c89ca4d7e46e70a31e8969
zachnurc <zach.nurcombe@gmail.com>
2018-04-27 20:03:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -383,7 +383,7 @@ class App extends Component {
               await getStationPlace(endStation).then(data=>{
                 for(var options = 0; options < data.length; options++){
                   if(data[options].station_code === endStation){
-                    console.log(end)
+                    console.log("end")
                     endLocationLat = data[options].latitude
                     endLocationLong = data[options].longitude
                   }
@@ -393,7 +393,7 @@ class App extends Component {
                 //loop through data to find right station code
                 for(var options = 0; options < data.length; options++){
                   if(data[options].station_code === startStation){
-                    console.log(start)
+                    console.log("start")
                     startLocationLat = data[options].latitude
                     startLocationLong = data[options].longitude
                   }
